75e1d361b3b60d6b270a81dc707e6e3fefe0b496
special0125 <ykt3401@naver.com>
2021-06-09 13:54:31 +0900

    update 2021-06-09

--- workspace/14_BATCH/src/batch/Top3StudentJob.java
+++ workspace/14_BATCH/src/batch/Top3StudentJob.java
@@ -1,6 +1,7 @@
 package batch;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
@@ -8,6 +9,7 @@ import java.util.List;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+
 
 import dao.StudentDAO;
 import dto.StudentDTO;
@@ -19,7 +21,13 @@ public class Top3StudentJob implements Job {
 		
 		List<StudentDTO> top3 = StudentDAO.getInstance().top3StudentList();
 		try {
-			BufferedWriter bw = new BufferedWriter(new FileWriter("포상자.txt"));
+			
+			File file = new File("D:" + File.separator +
+								 "spring0303" + File.separator + 
+								 "jspstudy" + File.separator + 
+								 "workspace" + File.separator +
+								 "14_BATCH", "포상자명단.txt");
+			BufferedWriter bw = new BufferedWriter(new FileWriter(file));
 			for (int i = 0; i < top3.size(); i++) {
 				bw.write((i + 1) + ", ");
 				bw.write(top3.get(i).getSno() + ", ");
